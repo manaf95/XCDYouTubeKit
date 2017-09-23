e78a05d60fa1f1c2ce333181737a05f2a8808e5b
manaf95 <manaf-95@hotmail.com>
2017-09-23 13:39:29 +0300

    fix control style

--- XCDYouTubeKit/XCDYouTubeVideoPlayerViewController.m
+++ XCDYouTubeKit/XCDYouTubeVideoPlayerViewController.m
@@ -139,7 +139,7 @@ NSString *const XCDYouTubeVideoUserInfoKey = @"Video";
 	
 	self.embedded = YES;
 	
-	self.moviePlayer.controlStyle = MPMovieControlStyleEmbedded;
+	self.moviePlayer.controlStyle = MPMovieControlStyleNone;
 	self.moviePlayer.view.frame = CGRectMake(0, 0, view.bounds.size.width, view.bounds.size.height);
 	self.moviePlayer.view.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
 	if (![view.subviews containsObject:self.moviePlayer.view])
